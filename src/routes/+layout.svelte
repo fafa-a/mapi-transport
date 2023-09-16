27ee845704ce15c0ec26e48694f6a8dcbe4b8c13
fafa-a <fabienpuech1@gmail.com>
2023-09-17 00:11:51 +0200

    feat: add layout & icon menu mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,44 @@
 <script>
   import '../app.css';
+  import { css } from 'styled-system/css';
+  import IconMenuMobile from '$lib/components/IconMenuMobile.svelte';
+  const title = 'mapi transport';
+  let open = false;
 </script>
 
+<header
+  class={css({
+    width: '100vw',
+    height: '3.5rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  })}
+>
+  <h1
+    class={css({
+      fontSize: '2xl',
+      textTransform: 'uppercase',
+      backgroundColor: 'white',
+      width: 'max-content',
+      height: '100%',
+      paddingLeft: '1rem',
+      paddingRight: '1rem',
+      display: 'grid',
+      placeItems: 'center'
+    })}
+  >
+    {title}
+  </h1>
+  <button
+    on:click={() => (open = !open)}
+    class={css({ marginRight: '2.0rem', height: '100%' })}
+  >
+    <IconMenuMobile width="2.5rem" height="100%" color="white" />
+  </button>
+  <!-- <ul> -->
+  <!--   <li><a href="/">Home</a></li> -->
+  <!--   <li><a href="/about">About</a></li> -->
+  <!-- </ul> -->
+</header>
 <slot />

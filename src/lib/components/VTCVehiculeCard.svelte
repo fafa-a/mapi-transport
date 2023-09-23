b06d53e55ab3de9cd4818da96427ae72d02c07f5
fafa-a <fabienpuech1@gmail.com>
2023-09-23 22:33:44 +0200

    feat: added VTC cards

--- src/lib/components/VTCVehiculeCard.svelte
+++ src/lib/components/VTCVehiculeCard.svelte
@@ -1,12 +1,88 @@
 <script lang="ts">
   import type { VehiculeCard } from '$lib/stores/mapi'
-
+  import { css } from 'styled-system/css'
+  import { hstack } from '../../../styled-system/patterns'
+  import IconPerson from './icon/IconPerson.svelte'
   export let vehicule: VehiculeCard
+  const { type, number_passager, reservation_required, price_minimum } =
+    vehicule
 </script>
 
-<div>
-  <div>{vehicule.type}{vehicule.number_passager}{vehicule.number_baggage}</div>
-  <div>{vehicule.reservation && vehicule.reservation}</div>
-  <div>{vehicule.imageURL}</div>
-  <div>{vehicule.price_minimum}</div>
+<div
+  class={css({
+    width: '100%',
+    height: '350px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    color: 'gray.800',
+    gap: '1rem',
+    mb: '1rem',
+    borderWidth: '1px',
+    rounded: 'md',
+    boxShadow: '2px 2px 2px 0px rgba(0,0,0,0.75)',
+    fontFamily: 'montserrat',
+    padding: '1.5rem 1.0rem'
+  })}
+>
+  <div class={css({ height: '3rem' })}>
+    <div
+      class={hstack({
+        justifyContent: 'space-between',
+        textTransform: 'capitalize'
+      })}
+    >
+      <p>{type}</p>
+      <div
+        class={css({
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center'
+        })}
+      >
+        <p>{number_passager}</p>
+        <IconPerson width="1.1rem" height="1.1rem" />
+      </div>
+    </div>
+
+    {#if reservation_required}
+      <p>Sur reservation</p>
+    {/if}
+  </div>
+  <img
+    class={css({
+      width: '100%',
+      height: '60%',
+      objectFit: 'contain',
+      transform: 'scale(1.1)'
+    })}
+    src={`src/lib/assets/images/vehicules/${type}-mobile.webp`}
+    srcset={`
+      src/lib/assets/images/vehicules/${type}-mobile.webp 420w,
+      src/lib/assets/images/vehicules/${type}-tablet.webp 768w,
+      src/lib/assets/images/vehicules/${type}-desktop.webp 1200w
+    `}
+    alt={type}
+  />
+  <div class={hstack({ justifyContent: 'space-around' })}>
+    <p>À partir de {price_minimum} €</p>
+    <a
+      class={css({
+        height: '100%',
+        backgroundColor: 'neutral.500',
+        color: 'white',
+        fontSize: 'lg',
+        padding: '0.2rem 2.0rem',
+        textTransform: 'capitalize',
+        display: 'grid',
+        placeItems: 'center',
+        rounded: 'lg',
+        fontFamily: 'nunito',
+        boxShadow: '4px 4px 4px 0px rgba(0,0,0,0.75)'
+      })}
+      href="/reservation"
+    >
+      Réserver
+    </a>
+  </div>
 </div>
--- src/lib/stores/mapi.ts
+++ src/lib/stores/mapi.ts
@@ -1,20 +1,19 @@
 export const SOCIETY_NAME = 'mapi transport'
 export const SENTENCE = 'Service de taxi sur : '
 export const CITIES = ['Toulouse', 'Marseille', 'Aix en Provence']
-export const PHONE_NUMBER = '+3376664227'
+export const PHONE_NUMBER = '+33766642277'
 
-enum TypeOfVehicule {
-  'BERLINE' = 'berline',
-  'VAN' = 'van',
-  'MINI_BUS' = 'mini-bus'
-}
+const TypeOfVehicule = {
+  BERLINE: 'berline',
+  VAN: 'van',
+  MINI_BUS: 'mini-bus'
+} as const
 
 export type VehiculeCard = {
-  type: TypeOfVehicule
+  type: (typeof TypeOfVehicule)[keyof typeof TypeOfVehicule]
   number_passager: number
-  number_baggage: number
-  reservation?: string | null
-  imageURL: string
+  number_baggage?: number
+  reservation_required?: boolean
   price_minimum: number
 }
 
@@ -22,22 +21,18 @@ export const VTC_VEHICULES: VehiculeCard[] = [
   {
     type: TypeOfVehicule.BERLINE,
     number_passager: 4,
-    number_baggage: 4,
-    imageURL: '',
     price_minimum: 20
   },
   {
     type: TypeOfVehicule.VAN,
-    number_passager: 6,
-    number_baggage: 6,
-    imageURL: '',
-    price_minimum: 50
+    number_passager: 7,
+    price_minimum: 50,
+    reservation_required: true
   },
   {
     type: TypeOfVehicule.MINI_BUS,
-    number_passager: 8,
-    number_baggage: 8,
-    imageURL: '',
-    price_minimum: 50
+    number_passager: 13,
+    price_minimum: 50,
+    reservation_required: true
   }
 ]

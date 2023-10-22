99e38f7babd2501d44d859338f9fc19caa8d0722
fafa-a <fabienpuech1@gmail.com>
2023-10-22 17:33:01 +0200

    chore : remove svelte cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare'
+// import adapter from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import { importAssets } from 'svelte-preprocess-import-assets'
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,23 +10,23 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      esbuildOptions: {
-        external: [
-          'fs',
-          'crypto',
-          'url',
-          'os',
-          'path',
-          'stream',
-          'zlib',
-          'net',
-          'tls',
-          'http',
-          'https'
-        ]
-      }
-    }),
+    // adapter: adapter({
+    //   esbuildOptions: {
+    //     external: [
+    //       'fs',
+    //       'crypto',
+    //       'url',
+    //       'os',
+    //       'path',
+    //       'stream',
+    //       'zlib',
+    //       'net',
+    //       'tls',
+    //       'http',
+    //       'https'
+    //     ]
+    //   }
+    // }),
     alias: {
       'styled-system': './styled-system/*'
     }
